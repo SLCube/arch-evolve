226ecd37adf18d6336fa6dbabe87b62f619482b1
SLCube <kgm0096@naver.com>
2025-11-02 22:43:37 +0900

    refactor: 전역 예외 핸들러에 로깅 기능 추가

--- src/main/kotlin/com/playground/common/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/playground/common/GlobalExceptionHandler.kt
@@ -1,6 +1,7 @@
 package com.playground.common
 
 import com.playground.common.error.ErrorResponse
+import com.playground.common.utils.logger
 import com.playground.product.exception.ProductNotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice
 class GlobalExceptionHandler {
 
+    private val log = logger()
+
     @ExceptionHandler(ProductNotFoundException::class)
     fun handleProductNotFoundException(e: ProductNotFoundException): ResponseEntity<ErrorResponse> {
+        log.error(e.message)
+
         val errorResponse = ErrorResponse(e.message ?: "상품을 찾을 수 없습니다.")
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse)
     }
