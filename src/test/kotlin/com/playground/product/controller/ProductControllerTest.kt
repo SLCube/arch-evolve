e5c93941e7c94c0e9a3bc9e8ecc35b374ccde0a1
SLCube <kgm0096@naver.com>
2025-11-03 20:45:01 +0900

    test: 재고 차감 기능에 대한 동시성 테스트 코드 작성

--- src/test/kotlin/com/playground/product/controller/ProductControllerTest.kt
+++ src/test/kotlin/com/playground/product/controller/ProductControllerTest.kt
@@ -6,6 +6,7 @@ import com.playground.product.controller.request.ProductSaveRequestDto
 import com.playground.product.controller.request.ProductUpdateRequestDto
 import com.playground.product.repository.ProductRepository
 import com.playground.product.service.ProductService
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -16,6 +17,8 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
 import org.springframework.test.web.servlet.put
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 
 @Suppress("NonAsciiCharacters")
 @AutoConfigureMockMvc
@@ -116,7 +119,8 @@ class ProductControllerTest(
         )
 
         mockMvc.put(
-            "/products/{id}", savedProduct.id) {
+            "/products/{id}", savedProduct.id
+        ) {
             contentType = MediaType.APPLICATION_JSON
             content = objectMapper.writeValueAsString(productUpdateRequestDto)
         }.andExpect {
@@ -126,7 +130,7 @@ class ProductControllerTest(
             jsonPath("$.errors.name") { value("상품 이름은 필수입니다.") }
         }
 
-}
+    }
 
     @Test
     fun `상품 수정시 재고가 0보다 작을경우`() {
@@ -150,69 +154,101 @@ class ProductControllerTest(
         }
     }
 
-@Test
-fun `상품을 조회한다`() {
-    val name = "상품1"
-    val stock = 10
-    val savedProduct = productService.save(name, stock)
+    @Test
+    fun `상품을 조회한다`() {
+        val name = "상품1"
+        val stock = 10
+        val savedProduct = productService.save(name, stock)
 
-    mockMvc.get("/products/{id}", savedProduct.id)
-        .andExpect {
-            status { isOk() }
-            jsonPath("$.id") { value(savedProduct.id) }
-            jsonPath("$.name") { value(name) }
-            jsonPath("$.stock") { value(stock) }
-        }
-}
-
-@Test
-fun `상품 조회를 실패한다`() {
-    val nonExistingId = 999L
-    mockMvc.get("/products/{id}", nonExistingId)
-        .andExpect {
-            status { isNotFound() }
-            jsonPath("$.code") { value(ErrorCode.PRODUCT_NOT_FOUND.code) }
-            jsonPath("$.message") { value(ErrorCode.PRODUCT_NOT_FOUND.message(nonExistingId)) }
-        }
-}
-
-@Test
-fun `상품 목록을 조회한다`() {
-    productService.save("상품1", 10)
-    productService.save("상품2", 20)
-
-    mockMvc.get("/products")
-        .andExpect {
-            status { isOk() }
-            jsonPath("$") { isArray() }
-            jsonPath("$.length()") { value(2) }
-            jsonPath("$[0].name") { value("상품1") }
-            jsonPath("$[0].stock") { value(10) }
-            jsonPath("$[1].name") { value("상품2") }
-            jsonPath("$[1].stock") { value(20) }
-        }
-}
-
-@Test
-fun `상품 정보를 수정한다`() {
-    val name = "상품1"
-    val stock = 10
-    val savedProduct = productService.save(name, stock)
-
-    val updatedName = "상품2"
-    val updatedStock = 20
-    val productUpdateRequestDto = ProductUpdateRequestDto(
-        name = updatedName,
-        stock = updatedStock
-    )
-
-    mockMvc.put("/products/{id}", savedProduct.id) {
-        contentType = MediaType.APPLICATION_JSON
-        content = objectMapper.writeValueAsString(productUpdateRequestDto)
-    }.andExpect {
-        status { isOk() }
-        jsonPath("$.name") { value(updatedName) }
-        jsonPath("$.stock") { value(updatedStock) }
+        mockMvc.get("/products/{id}", savedProduct.id)
+            .andExpect {
+                status { isOk() }
+                jsonPath("$.id") { value(savedProduct.id) }
+                jsonPath("$.name") { value(name) }
+                jsonPath("$.stock") { value(stock) }
+            }
     }
-}
+
+    @Test
+    fun `상품 조회를 실패한다`() {
+        val nonExistingId = 999L
+        mockMvc.get("/products/{id}", nonExistingId)
+            .andExpect {
+                status { isNotFound() }
+                jsonPath("$.code") { value(ErrorCode.PRODUCT_NOT_FOUND.code) }
+                jsonPath("$.message") { value(ErrorCode.PRODUCT_NOT_FOUND.message(nonExistingId)) }
+            }
+    }
+
+    @Test
+    fun `상품 목록을 조회한다`() {
+        productService.save("상품1", 10)
+        productService.save("상품2", 20)
+
+        mockMvc.get("/products")
+            .andExpect {
+                status { isOk() }
+                jsonPath("$") { isArray() }
+                jsonPath("$.length()") { value(2) }
+                jsonPath("$[0].name") { value("상품1") }
+                jsonPath("$[0].stock") { value(10) }
+                jsonPath("$[1].name") { value("상품2") }
+                jsonPath("$[1].stock") { value(20) }
+            }
+    }
+
+    @Test
+    fun `상품 정보를 수정한다`() {
+        val name = "상품1"
+        val stock = 10
+        val savedProduct = productService.save(name, stock)
+
+        val updatedName = "상품2"
+        val updatedStock = 20
+        val productUpdateRequestDto = ProductUpdateRequestDto(
+            name = updatedName,
+            stock = updatedStock
+        )
+
+        mockMvc.put("/products/{id}", savedProduct.id) {
+            contentType = MediaType.APPLICATION_JSON
+            content = objectMapper.writeValueAsString(productUpdateRequestDto)
+        }.andExpect {
+            status { isOk() }
+            jsonPath("$.name") { value(updatedName) }
+            jsonPath("$.stock") { value(updatedStock) }
+        }
+    }
+
+    @Test
+    fun `동시에 재고 100개를 차감한다`() {
+        val name = "상품1"
+        val stock = 100
+        val savedProduct = productService.save(name, stock)
+
+        val threadCount = 100
+        val executorService = Executors.newFixedThreadPool(32)
+
+        val latch = CountDownLatch(threadCount)
+
+        for (i in 1..threadCount) {
+            executorService.submit {
+                try {
+                    mockMvc.post("/products/{id}/decrease-stock", savedProduct.id) {
+                        param("quantity", "1")
+                    }.andExpect {
+                        status { isOk() }
+                    }
+                } finally {
+                    latch.countDown()
+                }
+            }
+        }
+
+        latch.await()
+
+        val finalProduct = productService.findById(savedProduct.id)
+
+        assertThat(finalProduct.stock).isZero()
+    }
 }
