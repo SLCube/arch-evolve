d15b7922d9b0617b53d6dfd72ca2c048218b91fe
SLCube <kgm0096@naver.com>
2025-11-08 15:27:07 +0900

    refactor: JwtConfig 설정 파일 추가 및 설정 관심사 분리

---
+++ src/main/kotlin/com/playground/common/security/config/JwtConfig.kt
@@ -0,0 +1,10 @@
+package com.playground.common.security.config
+
+import com.playground.common.security.jwt.JwtProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableConfigurationProperties(JwtProperties::class)
+class JwtConfig {
+}
