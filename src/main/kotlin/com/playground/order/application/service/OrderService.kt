100cca6e0c6c23b5c272eea467797ae56c0d322e
SLCube <kgm0096@naver.com>
2025-11-13 01:21:11 +0900

    refactor: OrderService에서 불필요한 사용자 조회 로직 제거

--- src/main/kotlin/com/playground/order/application/service/OrderService.kt
+++ src/main/kotlin/com/playground/order/application/service/OrderService.kt
@@ -9,8 +9,6 @@ import com.playground.order.domain.model.OrderProduct
 import com.playground.product.application.port.`in`.ProductUseCase
 import com.playground.product.application.port.`in`.command.DecreaseStockCommand
 import com.playground.product.application.port.`in`.query.GetProductQuery
-import com.playground.user.application.port.out.UserQueryPort
-import com.playground.user.domain.exception.UserNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -19,14 +17,10 @@ import org.springframework.transaction.annotation.Transactional
 class OrderService(
     private val orderCommandPort: OrderCommandPort,
     private val orderQueryPort: OrderQueryPort,
-    private val userQueryPort: UserQueryPort,
     private val productUseCase: ProductUseCase
 ): OrderUseCase {
 
     override fun createOrder(command: OrderCreateCommand): Order {
-        userQueryPort.findById(command.userId)
-            .orElseThrow { UserNotFoundException() }
-
         val order = Order(
             userId = command.userId,
             totalPrice = 0
