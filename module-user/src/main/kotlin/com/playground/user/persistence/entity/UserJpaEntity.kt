36dce1b571d7872e14c6cf83f3dc81e47364f3c8
강경모 <kgm0096@naver.com>
2025-12-01 13:54:45 +0900

    refactor(user): User Address 컬렉션 FetchType LAZY로 최적화

--- module-user/src/main/kotlin/com/playground/user/persistence/entity/UserJpaEntity.kt
+++ module-user/src/main/kotlin/com/playground/user/persistence/entity/UserJpaEntity.kt
@@ -8,6 +8,7 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -30,7 +31,8 @@ class UserJpaEntity(
 
     @OneToMany(
         cascade = [CascadeType.PERSIST, CascadeType.MERGE],
-        orphanRemoval = true
+        orphanRemoval = true, 
+        fetch = FetchType.LAZY
     )
     @JoinColumn(name = "user_id")
     val addressEntities: MutableList<UserAddressJpaEntity> = mutableListOf(),
