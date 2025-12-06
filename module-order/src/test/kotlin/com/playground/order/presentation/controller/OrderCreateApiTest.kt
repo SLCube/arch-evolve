ed1d01b60a4bd1d308e3a60b8c22fae773ff890e
SLCube <kgm0096@naver.com>
2025-12-06 13:23:40 +0900

    feat(test): 주문생성 - 성공 테스트에 spring rest docs snippet 추가

--- module-order/src/test/kotlin/com/playground/order/presentation/controller/OrderCreateApiTest.kt
+++ module-order/src/test/kotlin/com/playground/order/presentation/controller/OrderCreateApiTest.kt
@@ -16,13 +16,16 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.context.annotation.Import
 import org.springframework.http.HttpMethod
+import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
+import org.springframework.restdocs.payload.PayloadDocumentation.requestFields
+import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @Suppress("NonAsciiCharacters")
 @WebMvcTest(OrderController::class)
 @Import(OrderControllerTestConfig::class, TestSecurityConfig::class)
-class OrderCreateApiTest: RestDocsTest() {
+class OrderCreateApiTest : RestDocsTest() {
 
     @Autowired
     private lateinit var orderCommandUseCase: OrderCommandUseCase
@@ -58,6 +61,24 @@ class OrderCreateApiTest: RestDocsTest() {
                     jsonPath("$.orderProducts[1].productId").value(mockOrder.orderProducts[1].productId),
                     jsonPath("$.orderProducts[1].quantity").value(mockOrder.orderProducts[1].quantity),
                     jsonPath("$.orderProducts[1].price").value(mockOrder.orderProducts[1].price),
+                )
+            snippets =
+                arrayOf(
+                    requestFields(
+                        fieldWithPath("addressId").description("주소지 ID"),
+                        fieldWithPath("orderProducts[].productId").description("주문 상품 ID"),
+                        fieldWithPath("orderProducts[].quantity").description("주문 수량"),
+                    ),
+                    responseFields(
+                        fieldWithPath("id").description("주문 ID"),
+                        fieldWithPath("userId").description("주문 사용자 ID"),
+                        fieldWithPath("totalPrice").description("총 주문 금액"),
+                        fieldWithPath("status").description("주문 상태"),
+                        fieldWithPath("orderProducts[].id").description("주문 상품 ID"),
+                        fieldWithPath("orderProducts[].productId").description("상품 ID"),
+                        fieldWithPath("orderProducts[].quantity").description("주문 수량"),
+                        fieldWithPath("orderProducts[].price").description("주문 당시 상품 단가"),
+                    )
                 )
         }
     }
