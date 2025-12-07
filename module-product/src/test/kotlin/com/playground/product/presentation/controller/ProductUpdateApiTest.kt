aebfa03dd43dccb4bcb0b3900be78a8c10de0324
SLCube <kgm0096@naver.com>
2025-12-07 15:11:49 +0900

    refactor(test): ProductUpdateApiTest를 controller slice test로 전환

---
+++ module-product/src/test/kotlin/com/playground/product/presentation/controller/ProductUpdateApiTest.kt
@@ -0,0 +1,143 @@
+package com.playground.product.presentation.controller
+
+import com.playground.product.application.port.inbound.ProductUseCase
+import com.playground.product.fixture.application.domain.ProductDomainTestFixture
+import com.playground.product.fixture.presentation.request.ProductRequestTestFIxture
+import com.playground.product.presentation.annotation.ProductControllerSliceTest
+import com.playground.product.presentation.mapper.toCommand
+import com.playground.support.RestDocsTest
+import com.playground.support.docs.ApiDocumentUtils.commonErrorResponseSnippet
+import com.playground.support.docs.performAndDocument
+import com.playground.support.security.annotation.WithMockAuthUser
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.given
+import org.mockito.kotlin.never
+import org.mockito.kotlin.verify
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpMethod
+import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
+import org.springframework.restdocs.payload.PayloadDocumentation.requestFields
+import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@Suppress("NonAsciiCharacters")
+@ProductControllerSliceTest
+class ProductUpdateApiTest(
+    @param:Autowired private val productUseCase: ProductUseCase,
+): RestDocsTest() {
+
+    @Test
+    @WithMockAuthUser(role = "ADMIN")
+    fun `상품 수정 - 성공`() {
+        // given
+        val mockProductUpdateRequest = ProductRequestTestFIxture.mockProductUpdateRequest()
+        val mockUpdatedProduct = ProductDomainTestFixture.mockProduct(
+            name = mockProductUpdateRequest.name,
+            stock = mockProductUpdateRequest.stock,
+            price = mockProductUpdateRequest.price,
+        )
+        val mockCommand = mockProductUpdateRequest.toCommand(mockUpdatedProduct.id!!)
+
+        given(productUseCase.updateProduct(eq(mockCommand)))
+            .willReturn(mockUpdatedProduct)
+
+        // when & then
+        performAndDocument("상품 수정 - 성공") {
+            httpMethod = HttpMethod.PATCH
+            urlTemplate = "/products/{id}"
+            urlVars = arrayOf(mockUpdatedProduct.id)
+            requestBody = mockProductUpdateRequest
+            expectedStatus = status().isOk
+            additionalMatchers =
+                arrayOf(
+                    jsonPath("$.name").value(mockUpdatedProduct.name),
+                    jsonPath("$.stock").value(mockUpdatedProduct.stock),
+                    jsonPath("$.price").value(mockUpdatedProduct.price),
+                )
+            snippets =
+                arrayOf(
+                    requestFields(
+                        fieldWithPath("name").description("상품 이름"),
+                        fieldWithPath("stock").description("재고량"),
+                        fieldWithPath("price").description("상품 가격"),
+                    ),
+                    responseFields(
+                        fieldWithPath("id").description("상품 ID"),
+                        fieldWithPath("name").description("상품 이름"),
+                        fieldWithPath("stock").description("재고량"),
+                        fieldWithPath("price").description("상품 가격"),
+                    )
+                )
+        }
+    }
+
+    @Test
+    @WithMockAuthUser(role = "ADMIN")
+    fun `상품 수정 - 실패, 이름이 비어있음`() {
+        // given
+        val mockProductUpdateRequest = ProductRequestTestFIxture.mockProductUpdateRequest(
+            name = "",
+        )
+        val mockUpdatedProduct = ProductDomainTestFixture.mockProduct(
+            name = mockProductUpdateRequest.name,
+            stock = mockProductUpdateRequest.stock,
+            price = mockProductUpdateRequest.price,
+        )
+        val mockCommand = mockProductUpdateRequest.toCommand(mockUpdatedProduct.id!!)
+
+        given(productUseCase.updateProduct(eq(mockCommand)))
+            .willReturn(mockUpdatedProduct)
+
+        // when & then
+        performAndDocument("상품 수정 - 실패, 이름이 비어있음") {
+            httpMethod = HttpMethod.PATCH
+            urlTemplate = "/products/{id}"
+            urlVars = arrayOf(mockUpdatedProduct.id)
+            requestBody = mockProductUpdateRequest
+            expectedStatus = status().isBadRequest
+            additionalMatchers =
+                arrayOf(
+                    jsonPath("$.message").value("입력값이 유효하지 않습니다."),
+                    jsonPath("$.errors.name").value("상품 이름은 필수입니다.")
+                )
+            snippets =
+                arrayOf(
+                    responseFields(
+                        commonErrorResponseSnippet() +
+                                fieldWithPath("errors.name").description("상품 이름 필드의 에러 메세지")
+                    )
+                )
+        }
+    }
+
+    @Test
+    @WithMockAuthUser(role = "USER")
+    fun `상품 수정 - 실패, USER 권한으로 ADMIN API 접근 시도`() {
+        // given
+        val mockProductUpdateRequest = ProductRequestTestFIxture.mockProductUpdateRequest()
+        val mockUpdatedProduct = ProductDomainTestFixture.mockProduct(
+            name = mockProductUpdateRequest.name,
+            stock = mockProductUpdateRequest.stock,
+            price = mockProductUpdateRequest.price,
+        )
+
+        val productId = mockUpdatedProduct.id!!
+
+        performAndDocument("상품 수정 - 실패, USER 권한으로 ADMIN API 접근 시도") {
+            httpMethod = HttpMethod.PATCH
+            urlTemplate = "/products/{id}"
+            urlVars = arrayOf(productId)
+            requestBody = mockProductUpdateRequest
+            expectedStatus = status().isForbidden
+            snippets =
+                arrayOf(
+                    responseFields(commonErrorResponseSnippet()),
+                )
+        }
+
+        verify(productUseCase, never()).updateProduct(any())
+    }
+}
---
+++ module-product/src/test/kotlin/com/playground/product/presentation/request/ProductUpdateRequestValidateTest.kt
@@ -0,0 +1,59 @@
+package com.playground.product.presentation.request
+
+import com.playground.support.validate.ValidateTest
+import io.kotest.matchers.shouldBe
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.PositiveOrZero
+import org.junit.jupiter.api.Test
+
+@Suppress("NonAsciiCharacters")
+class ProductUpdateRequestValidateTest: ValidateTest() {
+
+    @Test
+    fun `상품 수정 시 이름이 비어있을 수 없다`() {
+        val request = ProductUpdateRequestDto(
+            name = "",
+            stock = 10,
+            price = 10000.toBigDecimal()
+        )
+
+        val validate = validator.validate(request)
+
+        validate.size shouldBe 1
+        val validation = validate.first()
+        validation.propertyPath.toString() shouldBe "name"
+        validation.constraintDescriptor.annotation.annotationClass.java shouldBe NotBlank::class.java
+    }
+
+    @Test
+    fun `상품 수정 시 재고가 0보다 작을 수 없다`() {
+        val request = ProductUpdateRequestDto(
+            name = "상품1",
+            stock = -1,
+            price = 10000.toBigDecimal()
+        )
+
+        val validate = validator.validate(request)
+
+        validate.size shouldBe 1
+        val validation = validate.first()
+        validation.propertyPath.toString() shouldBe "stock"
+        validation.constraintDescriptor.annotation.annotationClass.java shouldBe PositiveOrZero::class.java
+    }
+
+    @Test
+    fun `상품 수정 시 가격이 0보다 작을 수 없다`() {
+        val request = ProductUpdateRequestDto(
+            name = "상품1",
+            stock = 10,
+            price = (-1).toBigDecimal()
+        )
+
+        val validate = validator.validate(request)
+
+        validate.size shouldBe 1
+        val validation = validate.first()
+        validation.propertyPath.toString() shouldBe "price"
+        validation.constraintDescriptor.annotation.annotationClass.java shouldBe PositiveOrZero::class.java
+    }
+}
