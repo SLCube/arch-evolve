2dbdf8983947aa5c4a4a6a8988059533a2dc0c4c
SLCube <kgm0096@naver.com>
2025-12-09 19:55:37 +0900

    docs(user): UserInfo의 kdoc과 실제 코드가 맞지 않은점 수정

--- module-user-contract/src/main/kotlin/com/playground/user/contract/domain/vo/UserInfo.kt
+++ module-user-contract/src/main/kotlin/com/playground/user/contract/domain/vo/UserInfo.kt
@@ -6,10 +6,9 @@ package com.playground.user.contract.domain.vo
  * 이 DTO는 [User] 도메인이 외부에 노출하는 사용자 정보의 계약입니다.
  * 주로 다른 도메인(Order, Delivery 등)이 사용자 식별이나 기본 정보를 참조할 때 사용됩니다.
  *
- * 비밀번호와 같은 민감한 인증 정보는 포함하지 않습니다.
- *
  * @property userId 사용자의 고유 ID
  * @property loginId 사용자의 로그인 ID
+ * @property password 암호화된 패스워
  * @property role 사용자가 가진 권한 (단일 권한을 가정)
  */
 data class UserInfo(
