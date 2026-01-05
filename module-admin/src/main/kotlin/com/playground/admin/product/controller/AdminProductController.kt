8b733e0552edde9104ef41a945fa70060ec95387
SLCube <kgm0096@naver.com>
2026-01-05 22:02:44 +0900

    feat(admin-product): admin용 상품 조회/수정 뼈대 구성

---
+++ module-admin/src/main/kotlin/com/playground/admin/product/controller/AdminProductController.kt
@@ -0,0 +1,33 @@
+package com.playground.admin.product.controller
+
+import com.playground.admin.product.controller.request.AdminProductSaveRequestDto
+import com.playground.admin.product.controller.request.AdminProductUpdateRequestDto
+import com.playground.admin.product.service.AdminProductService
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/admin/products")
+class AdminProductController(
+    private val adminProductService: AdminProductService,
+) {
+    @PostMapping
+    fun save(
+        @RequestBody @Valid requestDto: AdminProductSaveRequestDto,
+    ) {
+        adminProductService.saveProduct()
+    }
+
+    @PatchMapping("/{id}")
+    fun update(
+        @PathVariable id: Long,
+        @RequestBody @Valid requestDto: AdminProductUpdateRequestDto,
+    ) {
+        adminProductService.updateProduct()
+    }
+}
---
+++ module-admin/src/main/kotlin/com/playground/admin/product/service/AdminProductService.kt
@@ -0,0 +1,15 @@
+package com.playground.admin.product.service
+
+import org.springframework.stereotype.Component
+
+@Component
+class AdminProductService {
+
+    fun saveProduct() {
+
+    }
+
+    fun updateProduct() {
+
+    }
+}
