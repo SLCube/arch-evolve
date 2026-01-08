53ff73be3c8a715212e974379abde5f9f8c0814c
SLCube <kgm0096@naver.com>
2026-01-08 21:18:22 +0900

    feat(admin-product): BackOfficeProductCommandUseCase 저장 수정후 로깅을 위한 이벤트 추가

--- module-product/src/main/kotlin/com/playground/product/application/service/backoffice/BackOfficeProductCommandService.kt
+++ module-product/src/main/kotlin/com/playground/product/application/service/backoffice/BackOfficeProductCommandService.kt
@@ -1,10 +1,13 @@
 package com.playground.product.application.service.backoffice
 
 import com.playground.product.application.port.outbound.ProductCommandPort
+import com.playground.product.application.port.outbound.ProductEventPort
 import com.playground.product.application.port.outbound.ProductQueryPort
 import com.playground.product.contract.application.port.inbound.backoffice.BackOfficeProductCommandUseCase
 import com.playground.product.contract.application.port.inbound.backoffice.command.AdminProductSaveCommand
 import com.playground.product.contract.application.port.inbound.backoffice.command.AdminProductUpdateCommand
+import com.playground.product.domain.event.ProductCreatedEvent
+import com.playground.product.domain.event.ProductUpdatedEvent
 import com.playground.product.domain.model.Product
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
@@ -14,6 +17,7 @@ import org.springframework.stereotype.Service
 class BackOfficeProductCommandService(
     private val productCommandPort: ProductCommandPort,
     private val productQueryPort: ProductQueryPort,
+    private val productEventPort: ProductEventPort,
 ): BackOfficeProductCommandUseCase {
     override fun saveProduct(command: AdminProductSaveCommand) {
         val product = Product(
@@ -23,10 +27,23 @@ class BackOfficeProductCommandService(
         )
 
         val savedProduct = productCommandPort.save(product)
+
+        val event = ProductCreatedEvent(
+            productId = savedProduct.id!!,
+            name = savedProduct.name,
+            stock = savedProduct.stock,
+            price = savedProduct.price,
+        )
+
+        productEventPort.publish(event)
     }
 
     override fun updateProduct(command: AdminProductUpdateCommand) {
         val product = productQueryPort.findById(command.id)
+
+        val oldName = product.name
+        val oldStock = product.stock
+        val oldPrice = product.price
 
         product.update(
             name = command.name,
@@ -35,5 +52,17 @@ class BackOfficeProductCommandService(
         )
 
         val updatedProduct = productCommandPort.update(product)
+
+        val event = ProductUpdatedEvent(
+            productId = updatedProduct.id!!,
+            oldName = oldName,
+            newName = updatedProduct.name,
+            oldStock = oldStock,
+            newStock = updatedProduct.stock,
+            oldPrice = oldPrice,
+            newPrice = updatedProduct.price,
+        )
+
+        productEventPort.publish(event)
     }
 }
