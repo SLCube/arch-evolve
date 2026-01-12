dda0c72c4199f4552e41368ceb271f7417c3aed6
SLCube <kgm0096@naver.com>
2026-01-12 22:17:06 +0900

    refactor(product): BackOfficeProductCommandService 의 transactional annotation을 jakarta에서 spring transactional로 변경
    - intellij의 자동완성으로 인해 jakarta transactional annotation을 사용하던걸 수정

--- module-product/src/main/kotlin/com/playground/product/application/service/backoffice/BackOfficeProductCommandService.kt
+++ module-product/src/main/kotlin/com/playground/product/application/service/backoffice/BackOfficeProductCommandService.kt
@@ -9,8 +9,8 @@ import com.playground.product.contract.application.port.inbound.backoffice.comma
 import com.playground.product.domain.event.ProductCreatedEvent
 import com.playground.product.domain.event.ProductUpdatedEvent
 import com.playground.product.domain.model.Product
-import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
