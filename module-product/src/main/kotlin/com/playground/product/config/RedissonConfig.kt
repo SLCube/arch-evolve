f0eb46af343c0c0c3d54a1d1a44006605e34396a
SLCube <kgm0096@naver.com>
2026-01-22 21:03:44 +0900

    refactor(redis): RedissonConfig에 ConfigurationProperties 방식으로 설정 개선

--- module-product/src/main/kotlin/com/playground/product/config/RedissonConfig.kt
+++ module-product/src/main/kotlin/com/playground/product/config/RedissonConfig.kt
@@ -3,22 +3,22 @@ package com.playground.product.config
 import org.redisson.Redisson
 import org.redisson.api.RedissonClient
 import org.redisson.config.Config
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ConditionalOnProperty(name = ["spring.data.redis.host"])
+@EnableConfigurationProperties(RedisProperties::class)
 class RedissonConfig(
-    @Value("\${spring.data.redis.host}") private val host: String,
-    @Value("\${spring.data.redis.port}") private val port: Int,
+    private val redisProperties: RedisProperties,
 ) {
     @Bean
     fun redissonClient(): RedissonClient {
         val config = Config()
         config.useSingleServer()
-            .setAddress("redis://$host:$port")
+            .setAddress("redis://${redisProperties.host}:${redisProperties.port}")
             .setConnectionPoolSize(50)
             .setConnectionMinimumIdleSize(10)
             .setTimeout(3000)
