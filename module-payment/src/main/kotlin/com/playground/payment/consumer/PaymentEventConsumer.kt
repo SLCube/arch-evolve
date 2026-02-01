2478e54b4543e22f2a564ca78b2231a633c581a0
SLCube <kgm0096@naver.com>
2026-02-01 17:03:24 +0900

    feat(payment): 결제 요청 이벤트 consumer를 transactional event listener after commit으로 변경

--- module-payment/src/main/kotlin/com/playground/payment/consumer/PaymentEventConsumer.kt
+++ module-payment/src/main/kotlin/com/playground/payment/consumer/PaymentEventConsumer.kt
@@ -3,17 +3,16 @@ package com.playground.payment.consumer
 import com.playground.order.contract.domain.event.OrderCreatedEvent
 import com.playground.payment.application.port.inbound.PaymentUseCase
 import com.playground.payment.application.port.inbound.command.PaymentAuthorizeCommand
-import org.springframework.context.event.EventListener
-import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
+import org.springframework.transaction.event.TransactionPhase
+import org.springframework.transaction.event.TransactionalEventListener
 
 @Component
 class PaymentEventConsumer(
     private val paymentUseCase: PaymentUseCase,
 ) {
 
-    @Order(2)
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun handleOrderCreatedEvent(event: OrderCreatedEvent) {
         paymentUseCase.authorizePayment(
             PaymentAuthorizeCommand(
