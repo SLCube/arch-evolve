d192b6360afe4e9ef42b274ee42376613b380900
SLCube <kgm0096@naver.com>
2026-02-03 20:32:08 +0900

    feat(k6): connection pool 100개로 증가 및 k6 부하테스트 완료
    - 더이상 모놀리식에서는 이 이상의 트래픽을 받아낼 방법이 없어보인다!!!

--- k6/scripts/order-constant-test.js
+++ k6/scripts/order-constant-test.js
@@ -6,11 +6,11 @@ export const options = {
     scenarios: {
         constant_load: {
             executor: "constant-arrival-rate",
-            rate: parseInt(__ENV.RATE || "100", 10), // 목표 RPS (기본 100)
+            rate: parseInt(__ENV.RATE || "700", 10), // 목표 RPS (기본 100)
             timeUnit: "1s",
             duration: __ENV.DURATION || "3m", // 테스트 지속 시간 (기본 3분)
-            preAllocatedVUs: parseInt(__ENV.PRE_VUS || "100", 10),
-            maxVUs: parseInt(__ENV.MAX_VUS || "200", 10),
+            preAllocatedVUs: parseInt(__ENV.PRE_VUS || "700", 10),
+            maxVUs: parseInt(__ENV.MAX_VUS || "1000", 10),
         },
     },
     thresholds: {
@@ -23,7 +23,7 @@ const BASE_URL = __ENV.BASE_URL || "http://localhost:8080";
 const LOGIN_PATH = __ENV.LOGIN_PATH || "/users/login";
 const ORDER_PATH = __ENV.ORDER_PATH || "/orders";
 
-const USER_COUNT = parseInt(__ENV.USER_COUNT || "200", 10);
+const USER_COUNT = parseInt(__ENV.USER_COUNT || "1000", 10);
 const PASSWORD = __ENV.PASSWORD || "testpassword";
 
 const PRODUCT_ID = parseInt(__ENV.PRODUCT_ID || "2", 10);
