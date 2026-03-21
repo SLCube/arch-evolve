bdc8e7a7ec80bc3d68acb656508b8d09fbf873ff
SLCube <kgm0096@naver.com>
2026-03-21 18:56:41 +0900

    lint(payment): ktlint 적용

--- payment-service/src/main/kotlin/com/playground/payment/presentation/support/UnauthorizedException.kt
+++ payment-service/src/main/kotlin/com/playground/payment/presentation/support/UnauthorizedException.kt
@@ -3,7 +3,8 @@ package com.playground.payment.presentation.support
 import com.playground.payment.domain.exception.BusinessException
 import com.playground.payment.domain.exception.ErrorCode
 
-class UnauthorizedException : BusinessException(
-    errorCode = ErrorCode.UNAUTHORIZED,
-    message = ErrorCode.UNAUTHORIZED.message(),
-)
+class UnauthorizedException :
+    BusinessException(
+        errorCode = ErrorCode.UNAUTHORIZED,
+        message = ErrorCode.UNAUTHORIZED.message(),
+    )
